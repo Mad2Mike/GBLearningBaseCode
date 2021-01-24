a36182da9fcd7c84978c2fc91aed846528855023
Mike <mad3mike@gmail.com>
2021-01-24 03:50:56 +0300

    Signed-off-by: Mike <mad3mike@gmail.com>

---
+++ PlayerStats.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Geekbrains
+{
+
+    public class PlayerStats : MonoBehaviour
+    {
+
+
+        public float _basePlayerHP;
+        public float _basePlayerEnergy;
+
+
+        void Start()
+        {
+            _basePlayerHP = 100f;
+            _basePlayerEnergy = 100f;
+        }
+
+    }
+
+}
---
+++ SpeedPotion.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Geekbrains
+{
+    public class SpeedPotion : MonoBehaviour
+    {
+        [SerializeField] private BonusList BonusList;
+
+
+        void OnTriggerEnter(Collider player)
+        {
+            if (player.gameObject.tag == "Player")
+            {
+
+                BonusList.SpeedUp(10,50);
+
+
+            }
+        }
+    }
+}
